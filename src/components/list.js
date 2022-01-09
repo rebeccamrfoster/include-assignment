199c1e78b8d2135955a22783b5641c4e8eb3217d
rebeccamrfoster <rebeccamrfoster@gmail.com>
2022-01-09 18:12:32 -0500

    Rename App.js to list.js, incorporate form rather than extracting it into own component

---
+++ src/components/list.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import "../stylesheets/list.css";
+import ListItem from "./list_item";
+
+export default function App() {
+    const [list, setList] = useState([]);
+    const [name, setName] = useState("");
+    const [number, setNumber] = useState(0);
+    const [checked, setChecked] = useState(false);
+
+    const updateName = event => setName(event.target.value);
+    const updateNumber = event => setNumber(event.target.value);
+    const updateChecked = event => setChecked(!checked);
+
+    const handleSubmit = event => {
+        const newItem = { "name": name, "number": number, "checked": checked };
+        setList(list.concat(newItem));
+        setName("");
+        setNumber(0);
+        setChecked(false);
+    };
+
+    return (
+        <div>
+            <h1>Travel Checklist</h1>
+            <form>
+                <label>Item
+                    <input type="text" value={name} onChange={updateName} />
+                </label>
+                <label>Quantity
+                    <input type="number" value={number} onChange={updateNumber} />
+                </label>
+                <label>Packed
+                    <input type="checkbox" checked={checked ? "checked" : ""}
+                        onChange={updateChecked} />
+                </label>
+
+                <input type="submit" value="Add New Item"
+                    onClick={handleSubmit} />
+            </form>
+
+            <table>
+                <tr>
+                    <th>Item</th>
+                    <th>Quantity</th>
+                    <th>Packed</th>
+                </tr>
+                {
+                    list.map(item => <ListItem name={item.name}
+                        number={item.number}
+                        packed={item.checked} />
+                    )
+                }
+            </table>
+        </div>
+    );
+}
